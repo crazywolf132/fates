1e8696ee24f76941bf0a15890ca4c03e08e92102
crazywolf132 <crazywolf132@gmail.com>
2024-11-11 18:36:33 +1100

    fix: re-added `all` utility

---
+++ src/utils/all.ts
@@ -0,0 +1,23 @@
+import type { Result, AsyncResult } from "../result";
+
+/**
+ * Combines multiple Results or AsyncResults into a single AsyncResult.
+ * Returns Ok only if all Results are Ok, otherwise returns the first Err encountered.
+ * Similar to Promise.all but for Results.
+ */
+export function all<T extends any[], E>(results: {
+    [K in keyof T]: Result<T[K], E> | AsyncResult<T[K], E>;
+  }): AsyncResult<T, E> {
+    return Promise.all(
+      results.map((r) => (r instanceof Promise ? r : Promise.resolve(r)))
+    ).then((resolvedResults) => {
+      const values: any[] = [];
+      for (const result of resolvedResults) {
+        if (result.isErr()) {
+          return result as Result<T, E>;
+        }
+        values.push(result.unwrap());
+      }
+      return ok(values as T);
+    });
+  }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
-export * from './chain';
-export * from './collect';
-export * from './lazy';
-export * from './match';
-export * from './parse';
-export * from './pipeline';
-export * from './try';
-
+export * from "./chain";
+export * from "./collect";
+export * from "./lazy";
+export * from "./match";
+export * from "./parse";
+export * from "./pipeline";
+export * from "./try";
+export * from "./all";
